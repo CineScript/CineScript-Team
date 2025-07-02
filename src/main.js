e712209d46d7951a1f388df60537750a62dee55b
begumnarmanli <begumnarmanli@gmail.com>
2025-07-02 21:45:16 +0300

    Değişiklikler yapıldı

--- src/main.js
+++ src/main.js
@@ -9,20 +9,3 @@
 // import { ... } from './library.js';
 // import { ... } from './footer.js';
 
-const themeButtons = document.querySelectorAll('[data-theme-option]');
-const htmlEl = document.documentElement;
-
-// Kaydedilmiş tema varsa uygula
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme) {
-  htmlEl.setAttribute('data-theme', savedTheme);
-}
-
-// Butonlara tıklama dinleyicisi
-themeButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const selectedTheme = button.getAttribute('data-theme-option');
-    htmlEl.setAttribute('data-theme', selectedTheme);
-    localStorage.setItem('theme', selectedTheme);
-  });
-});
