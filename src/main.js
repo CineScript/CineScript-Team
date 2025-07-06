1794440523b5b463d0d360bad8a9c18221b5605a
begumnarmanli <159467416+begumnarmanli@users.noreply.github.com>
2025-07-06 18:14:41 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,51 +1,30 @@
-// burada ... kısmına yazılan js dosyalarındaki aktarılacak tanım isimleri gelecek. isimler virgül ile ayrılmalıdır.
-// import { ... } from './header.js';
-// import { ... } from './hero.js';
-// import { ... } from './catalog-hero.js';
-import './js/library-hero.js';
-// import { ... } from './trends.js';
-import './js/upcoming.js';
-// import { ... } from './catalog.js';
-// import { ... } from './library.js';
-import './js/footer.js';
-
-import 'izitoast/dist/css/iziToast.min.css';
-
-document.addEventListener('DOMContentLoaded', renderUpcoming);
-
 import { renderUpcoming } from './js/upcoming.js';
-
+import './js/header.js';
+import './js/hero.js';
+import './js/catalog-hero.js';
+import './js/library-hero.js';
+import './js/trends.js';
+// import './js/upcoming.js';
+import './js/catalog.js';
+import './js/library.js';
+import './js/footer.js';
+import './js/popup.js';
+import './js/loader.js';
+import './js/scroll-up.js';
+import './api/tmdbApi.js';
+import 'izitoast/dist/css/iziToast.min.css';
 document.addEventListener('DOMContentLoaded', () => {
   const upcomingSection = document.getElementById('upcoming');
   if (upcomingSection) {
     renderUpcoming();
   }
 });
-
 import { initCatalog } from './js/catalog.js';
 import { createMoviePopup } from './js/popup.js';
-
 import { showLoader, hideLoader } from './js/loader.js';
 import './js/scroll-up.js';
 import './js/library-hero.js';
 import './js/library.js';
-
-const themeButtons = document.querySelectorAll('[data-theme-option]');
-const htmlEl = document.documentElement;
-
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme) {
-  htmlEl.setAttribute('data-theme', savedTheme);
-}
-
-themeButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const selectedTheme = button.getAttribute('data-theme-option');
-    htmlEl.setAttribute('data-theme', selectedTheme);
-    localStorage.setItem('theme', selectedTheme);
-  });
-});
-
 async function loadPartials() {
   const loads = document.querySelectorAll('load');
   for (const el of loads) {
@@ -63,7 +42,4 @@ async function loadPartials() {
     }
   }
 }
-
-loadPartials().then(() => {
-  initCatalog();
-});
+loadPartials().then(() => {});
