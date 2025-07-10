28c0441890c29f63f1a708debabf958e3d3ecc99
furkycl <81173127+furkycl@users.noreply.github.com>
2025-07-10 23:30:50 +0000

    Deploying to gh-pages from @ CineScript/CineScript-Team@54dfb2d11ad909d777c0595b0a42318f7ad255cf 🚀

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import{a as E,b as B,c as $,d as I,e as O,g as U}from"./assets/catalog-BS3-rZm0.js";import{b as k}from"./assets/vendor-PRc6rJ_y.js";import"./assets/main-B4ioPE5y.js";function A(s){document.querySelectorAll(".basicLightbox").forEach(t=>t.remove());const r=k.create(s);r.show();const c=r.element();c.addEventListener("click",t=>{t.target===c&&r.close()},{once:!0})}async function q(){const s=document.getElementById("hero1"),r=document.getElementById("hero2");try{const t=(await E()).results[0],g=await B(t.id);if(g.results.some(n=>n.type==="Trailer"&&n.site==="YouTube")&&t.backdrop_path){r.style.backgroundImage=`url(https://image.tmdb.org/t/p/original${t.backdrop_path})`,r.style.backgroundSize="cover",r.style.backgroundPosition="center",r.style.backgroundRepeat="no-repeat",s.style.display="none",r.style.display="block",r.querySelector(".hero2-title").textContent=t.title,r.querySelector(".hero2-description").textContent=t.overview||"No description available.";const n=t.vote_average,u=Math.floor(n/2),v=n%2>=1?1:0,e=5-u-v,o=r.querySelector(".hero2-stars");o.innerHTML="";for(let a=0;a<u;a++){const l=document.createElement("img");l.src="./svg/star.svg",l.alt="full star",l.classList.add("star-icon"),o.appendChild(l)}if(v){const a=document.createElement("img");a.src="./svg/star-half.svg",a.alt="half star",a.classList.add("star-icon"),o.appendChild(a)}for(let a=0;a<e;a++){const l=document.createElement("img");l.src="./svg/star-outline.svg",l.alt="empty star",l.classList.add("star-icon"),o.appendChild(l)}const i=document.getElementById("watch-trailer-btn");i&&i.addEventListener("click",async()=>{try{const a=g.results.find(l=>l.type==="Trailer"&&l.site==="YouTube");if(a){const l=a.key;A(`
+import{a as I,b as O,c as C,d as A,e as U,g as q}from"./assets/catalog-AJGnDrES.js";import{b as S}from"./assets/vendor-PRc6rJ_y.js";import"./assets/main-CCUjFcIn.js";function T(a,t){let n=JSON.parse(localStorage.getItem("library"))||[];n.includes(a.id)?(n=n.filter(o=>o!==a.id),t.textContent="Add to Library",t.classList.remove("remove-from-library"),t.classList.add("add-to-library")):(n.push(a.id),t.textContent="Remove from my library",t.classList.remove("add-to-library"),t.classList.add("remove-from-library")),localStorage.setItem("library",JSON.stringify(n))}function _(a,t){(JSON.parse(localStorage.getItem("library"))||[]).includes(a.id)?(t.textContent="Remove from my library",t.classList.add("remove-from-library")):(t.textContent="Add to Library",t.classList.add("add-to-library"))}function N(a){document.querySelectorAll(".basicLightbox").forEach(r=>r.remove());const t=S.create(a);t.show();const n=t.element();n.addEventListener("click",r=>{r.target===n&&t.close()},{once:!0})}async function H(){const a=document.getElementById("hero1"),t=document.getElementById("hero2");try{const n=await I(),r=Math.floor(Math.random()*n.results.length),o=n.results[r],u=await O(o.id);if(u.results.some(d=>d.type==="Trailer"&&d.site==="YouTube")&&o.backdrop_path){t.style.backgroundImage=`url(https://image.tmdb.org/t/p/original${o.backdrop_path})`,t.style.backgroundSize="cover",t.style.backgroundPosition="center",t.style.backgroundRepeat="no-repeat",a.style.display="none",t.style.display="block",t.querySelector(".hero2-title").textContent=o.title,t.querySelector(".hero2-description").textContent=o.overview||"No description available.";const d=o.vote_average,f=Math.floor(d/2),e=d%2>=1?1:0,s=5-f-e,c=t.querySelector(".hero2-stars");c.innerHTML="";for(let i=0;i<f;i++){const l=document.createElement("img");l.src="./svg/star.svg",l.alt="full star",l.classList.add("star-icon"),c.appendChild(l)}if(e){const i=document.createElement("img");i.src="./svg/star-half.svg",i.alt="half star",i.classList.add("star-icon"),c.appendChild(i)}for(let i=0;i<s;i++){const l=document.createElement("img");l.src="./svg/star-outline.svg",l.alt="empty star",l.classList.add("star-icon"),c.appendChild(l)}const m=document.getElementById("watch-trailer-btn");m&&m.addEventListener("click",async()=>{try{const i=u.results.find(l=>l.type==="Trailer"&&l.site==="YouTube");if(i){const l=i.key;N(`
                 <div class="popup-video-wrapper">
                   <iframe
                     width="800"
@@ -9,7 +9,7 @@ import{a as E,b as B,c as $,d as I,e as O,g as U}from"./assets/catalog-BS3-rZm0.
                     allowfullscreen
                   ></iframe>
                 </div>
-              `)}}catch(a){console.error("Video açılırken hata:",a)}});const m=r.querySelector("#more-details-btn");m&&m.addEventListener("click",async()=>{try{const l=(await $()).genres.reduce((p,h)=>(p[h.id]=h.name,p),{}),b=t.poster_path?`https://image.tmdb.org/t/p/w500${t.poster_path}`:"https://via.placeholder.com/500x750?text=No+Image",y=t.genre_ids.map(p=>l[p]).join(", ");k.create(`
+              `)}}catch(i){console.error("Video açılırken hata:",i)}});const y=t.querySelector("#more-details-btn");y&&y.addEventListener("click",async()=>{try{const l=(await C()).genres.reduce((p,w)=>(p[w.id]=w.name,p),{}),h=o.poster_path?`https://image.tmdb.org/t/p/w500${o.poster_path}`:"https://via.placeholder.com/500x750?text=No+Image",b=o.genre_ids.map(p=>l[p]).join(", ");S.create(`
               <div class="movie-modal">
                 <button class="popup-close-btn" aria-label="Close">
                   <svg class="popup-close-icon" width="24" height="24" viewBox="0 0 24 24" fill="none">
@@ -18,18 +18,18 @@ import{a as E,b as B,c as $,d as I,e as O,g as U}from"./assets/catalog-BS3-rZm0.
                   </svg>
                 </button>
 
-                <img src="${b}" class="modal-poster" alt="${t.title}">
+                <img src="${h}" class="modal-poster" alt="${o.title}">
                 <div class="modal-details">
-                  <h2>${t.title}</h2>
-                  <p><strong>Vote / Votes:</strong> ${t.vote_average} / ${t.vote_count}</p>
-                  <p><strong>Popularity:</strong> ${t.popularity}</p>
-                  <p><strong>Genre:</strong> ${y}</p>
+                  <h2>${o.title}</h2>
+                  <p><strong>Vote / Votes:</strong> ${o.vote_average} / ${o.vote_count}</p>
+                  <p><strong>Popularity:</strong> ${o.popularity}</p>
+                  <p><strong>Genre:</strong> ${b}</p>
                   <h3>ABOUT</h3>
-                  <p>${t.overview}</p>
+                  <p>${o.overview}</p>
                   <button class="add-to-library">Add to Library</button>
                 </div>
               </div>
-              `,{onShow:p=>{p.element().querySelector(".popup-close-btn").addEventListener("click",()=>p.close())}}).show()}catch(a){console.error("Detay popup gösterilemedi:",a)}})}else s.style.display="block",r.style.display="none"}catch(c){console.error("Hata oluştu:",c),s.style.display="block",r.style.display="none"}}document.addEventListener("DOMContentLoaded",()=>{q();const s=document.querySelector("#hero1 .hero-button");s&&s.addEventListener("click",()=>{const t=`
+              `,{onShow:p=>{p.element().querySelector(".popup-close-btn").addEventListener("click",()=>p.close());const L=p.element().querySelector(".add-to-library");_(o,L),L.addEventListener("click",()=>{T(o,L)})}}).show()}catch(i){console.error("Detay popup gösterilemedi:",i)}})}else a.style.display="block",t.style.display="none"}catch(n){console.error("Hata oluştu:",n),a.style.display="block",t.style.display="none"}}document.addEventListener("DOMContentLoaded",()=>{H();const a=document.querySelector("#hero1 .hero-button");a&&a.addEventListener("click",()=>{const r=`
         <div class="popup-wrapper">
           <button class="popup-close-btn" aria-label="Close">
             <svg class="popup-close-icon" width="24" height="24" viewBox="0 0 24 24" fill="none">
@@ -44,52 +44,52 @@ import{a as E,b as B,c as $,d as I,e as O,g as U}from"./assets/catalog-BS3-rZm0.
           </p>
           <img src="${document.documentElement.getAttribute("data-theme")==="dark"?"./img/camera-dark.png":"./img/camera-light.png"}" alt="Camera icon" class="popup-img" />
         </div>
-      `;k.create(t,{onShow:d=>{d.element().querySelector(".popup-close-btn").addEventListener("click",()=>d.close())}}).show()})});const M=document.querySelector(".weekly-gallery"),H=document.querySelector(".weekly-see-all");let S=[],L=!1;function x(){const s=window.innerWidth;return s<768?1:(s<1280,3)}function N(s){const r=Math.floor(s/2),c=s%2>=1,t=5-r-(c?1:0),g=[];for(let d=0;d<r;d++)g.push('<img src="./svg/star.svg" alt="star" />');c&&g.push('<img src="./svg/star-half.svg" alt="half star" />');for(let d=0;d<t;d++)g.push('<img src="./svg/star-outline.svg" alt="empty star" />');return g.join("")}async function D(s=x()){try{const[r,c]=await Promise.all([O(),$()]);S=r.results;const t=c.genres.reduce((n,u)=>(n[u.id]=u.name,n),{}),d=S.slice(0,s).map(n=>{const u=n.poster_path?`https://image.tmdb.org/t/p/w500${n.poster_path}`:"https://via.placeholder.com/500x750?text=No+Image",v=n.genre_ids.map(i=>t[i]).slice(0,1).join(", "),e=n.release_date?n.release_date.slice(0,4):"N/A",o=N(n.vote_average);return`
-        <li class="weekly-card" data-id="${n.id}">
+      `;S.create(r,{onShow:u=>{u.element().querySelector(".popup-close-btn").addEventListener("click",()=>u.close())}}).show()})});const $=document.querySelector(".weekly-gallery"),P=document.querySelector(".weekly-see-all");let x=[],M=!1;function B(){const a=window.innerWidth;return a<768?1:(a<1280,3)}function V(a){const t=Math.floor(a/2),n=a%2>=1,r=5-t-(n?1:0),o=[];for(let u=0;u<t;u++)o.push('<img src="./svg/star.svg" alt="star" />');n&&o.push('<img src="./svg/star-half.svg" alt="half star" />');for(let u=0;u<r;u++)o.push('<img src="./svg/star-outline.svg" alt="empty star" />');return o.join("")}async function D(a=B()){try{const[t,n]=await Promise.all([U(),C()]);x=t.results;const r=n.genres.reduce((g,d)=>(g[d.id]=d.name,g),{}),u=x.slice(0,a).map(g=>{const d=g.poster_path?`https://image.tmdb.org/t/p/w500${g.poster_path}`:"https://via.placeholder.com/500x750?text=No+Image",f=g.genre_ids.map(c=>r[c]).slice(0,1).join(", "),e=g.release_date?g.release_date.slice(0,4):"N/A",s=V(g.vote_average);return`
+        <li class="weekly-card" data-id="${g.id}">
           <div class="poster-wrapper">
-            <img class="card-img" src="${u}" alt="${n.title}" />
+            <img class="card-img" src="${d}" alt="${g.title}" />
             <div class="card-overlay">
-              <h3 class="card-title">${n.title.toUpperCase()}</h3>
-              <p class="card-info">${v} | ${e}</p>
-              <div class="card-rating">${o}</div>
+              <h3 class="card-title">${g.title.toUpperCase()}</h3>
+              <p class="card-info">${f} | ${e}</p>
+              <div class="card-rating">${s}</div>
             </div>
           </div>
         </li>
-        `}).join("");M.innerHTML=d}catch(r){console.error("Weekly trends fetch error:",r),M.innerHTML="<p>Veriler alınamadı.</p>"}}H.addEventListener("click",()=>{L=!L;const s=L?S.length:x();D(s)});M.addEventListener("click",async s=>{const r=s.target.closest(".weekly-card");if(!r)return;const c=r.dataset.id;try{const t=await I(c),g=t.poster_path?`https://image.tmdb.org/t/p/w500${t.poster_path}`:"https://via.placeholder.com/500x750?text=No+Image",d=t.genres.map(u=>u.name).join(", ");k.create(`
-        <div class="movie-modal">
-    <button class="popup-close-btn" aria-label="Close">
-      <svg class="popup-close-icon" width="24" height="24" viewBox="0 0 24 24" fill="none">
-        <path d="M6 6L18 18" stroke="currentColor" stroke-width="2"/>
-        <path d="M6 18L18 6" stroke="currentColor" stroke-width="2"/>
-      </svg>
-    </button>
+        `}).join("");$.innerHTML=u}catch(t){console.error("Weekly trends fetch error:",t),$.innerHTML="<p>Veriler alınamadı.</p>"}}P.addEventListener("click",()=>{M=!M;const a=M?x.length:B();D(a)});$.addEventListener("click",async a=>{const t=a.target.closest(".weekly-card");if(!t)return;const n=t.dataset.id;try{const r=await A(n),o=r.poster_path?`https://image.tmdb.org/t/p/w500${r.poster_path}`:"https://via.placeholder.com/500x750?text=No+Image",u=r.genres.map(d=>d.name).join(", ");S.create(`
+      <div class="movie-modal">
+        <button class="popup-close-btn" aria-label="Close">
+          <svg class="popup-close-icon" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <path d="M6 6L18 18" stroke="currentColor" stroke-width="2"/>
+            <path d="M6 18L18 6" stroke="currentColor" stroke-width="2"/>
+          </svg>
+        </button>
 
-    <img src="${g}" class="modal-poster" alt="${t.title}">
-    <div class="modal-details">
-      <h2>${t.title}</h2>
-      <p><strong>Vote / Votes:</strong> ${t.vote_average} / ${t.vote_count}</p>
-      <p><strong>Popularity:</strong> ${t.popularity}</p>
-      <p><strong>Genre:</strong> ${d}</p>
-      <h3>ABOUT</h3>
-      <p>${t.overview}</p>
-      <button class="add-to-library">Add to Library</button>
-    </div>
-  </div>
-    `,{onShow:u=>{u.element().querySelector(".popup-close-btn").addEventListener("click",()=>u.close())}}).show()}catch(t){console.error("Popup açılırken hata:",t)}});D();const w=document.querySelector(".upcoming-container");document.addEventListener("DOMContentLoaded",async()=>{let s=new Map;async function r(){try{const e=await $();e&&e.genres&&e.genres.forEach(o=>{s.set(o.id,o.name)})}catch(e){console.error("Türler çekilirken bir hata oluştu:",e)}}function c(e){return!e||e.length===0?"Unknown":e.map(o=>s.get(o)||"Unknown").join(", ")}function t(){const e=localStorage.getItem("library");return e?JSON.parse(e):[]}function g(e){localStorage.setItem("library",JSON.stringify(e))}function d(e,o){let i=t();const m=e.id;i.includes(m)?(i=i.filter(a=>a!==m),o.textContent="Add to my library",o.classList.remove("removed")):(i.push(m),o.textContent="Remove from my library",o.classList.add("removed")),g(i)}function n(e,o){t().includes(e.id)?(o.textContent="Remove from my library",o.classList.add("removed")):(o.textContent="Add to my library",o.classList.remove("removed"))}async function u(){await r();const e=new Date,o=e.getFullYear(),i=e.getMonth(),a=new Date(o,i,1).toISOString().split("T")[0],b=new Date(o,i+1,0).toISOString().split("T")[0];try{const y=await U(a,b);if(y&&y.results&&y.results.length>0){const f=y.results.filter(p=>{const h=new Date(p.release_date);return h.getFullYear()===o&&h.getMonth()===i});if(f.length>0){const p=Math.floor(Math.random()*f.length),h=f[p];v(h)}else w.innerHTML="<p>No movie released this month.</p>"}else w.innerHTML="<p>No upcoming movies found.</p>"}catch(y){console.error("Yaklaşan filmler çekilirken bir hata oluştu:",y),w.innerHTML="<p>There was an error loading movies. Please try again later.</p>"}}function v(e){w.innerHTML="";const o=document.createElement("div");o.classList.add("upcoming-item");const i=e.poster_path?`https://image.tmdb.org/t/p/w1280${e.poster_path}`:"https://via.placeholder.com/150x225?text=Poster+Yok",m=e.release_date;let a="Unknown";if(m){const h=new Date(m),T=String(h.getDate()).padStart(2,"0"),_=String(h.getMonth()+1).padStart(2,"0"),C=h.getFullYear();a=`${T}.${_}.${C}`}const l=e.vote_average?e.vote_average.toFixed(2):"Unknown",b=e.popularity?e.popularity.toFixed(2):"Unknown",y=e.overview||"There is no information about this movie.",f=c(e.genre_ids);o.innerHTML=`
+        <img src="${o}" class="modal-poster" alt="${r.title}">
+        <div class="modal-details">
+          <h2>${r.title}</h2>
+          <p><strong>Vote / Votes:</strong> ${r.vote_average} / ${r.vote_count}</p>
+          <p><strong>Popularity:</strong> ${r.popularity}</p>
+          <p><strong>Genre:</strong> ${u}</p>
+          <h3>ABOUT</h3>
+          <p>${r.overview}</p>
+          <button class="add-to-library">Add to Library</button>
+        </div>
+      </div>
+      `,{onShow:d=>{d.element().querySelector(".popup-close-btn").addEventListener("click",()=>d.close());const e=d.element().querySelector(".add-to-library");_(r,e),e.addEventListener("click",()=>{T(r,e)})}}).show()}catch(r){console.error("Popup açılırken hata:",r)}});D();const k=document.querySelector(".upcoming-container");document.addEventListener("DOMContentLoaded",async()=>{let a=new Map;async function t(){try{const e=await C();e&&e.genres&&e.genres.forEach(s=>{a.set(s.id,s.name)})}catch(e){console.error("Türler çekilirken bir hata oluştu:",e)}}function n(e){return!e||e.length===0?"Unknown":e.map(s=>a.get(s)||"Unknown").join(", ")}function r(){const e=localStorage.getItem("library");return e?JSON.parse(e):[]}function o(e){localStorage.setItem("library",JSON.stringify(e))}function u(e,s){let c=r();const m=e.id;c.includes(m)?(c=c.filter(y=>y!==m),s.textContent="Add to my library",s.classList.remove("removed")):(c.push(m),s.textContent="Remove from my library",s.classList.add("removed")),o(c)}function g(e,s){r().includes(e.id)?(s.textContent="Remove from my library",s.classList.add("removed")):(s.textContent="Add to my library",s.classList.remove("removed"))}async function d(){await t();const e=new Date,s=e.getFullYear(),c=e.getMonth(),y=new Date(s,c,1).toISOString().split("T")[0],l=new Date(s,c+1,0).toISOString().split("T")[0];try{const h=await q(y,l);if(h&&h.results&&h.results.length>0){const b=h.results.filter(v=>{const p=new Date(v.release_date);return p.getFullYear()===s&&p.getMonth()===c});if(b.length>0){const v=Math.floor(Math.random()*b.length),p=b[v];f(p)}else k.innerHTML="<p>No movie released this month.</p>"}else k.innerHTML="<p>No upcoming movies found.</p>"}catch(h){console.error("Yaklaşan filmler çekilirken bir hata oluştu:",h),k.innerHTML="<p>There was an error loading movies. Please try again later.</p>"}}function f(e){k.innerHTML="";const s=document.createElement("div");s.classList.add("upcoming-item");const c=e.poster_path?`https://image.tmdb.org/t/p/w1280${e.poster_path}`:"https://via.placeholder.com/150x225?text=Poster+Yok",m=e.release_date;let y="Unknown";if(m){const p=new Date(m),w=String(p.getDate()).padStart(2,"0"),L=String(p.getMonth()+1).padStart(2,"0"),E=p.getFullYear();y=`${w}.${L}.${E}`}const i=e.vote_average?e.vote_average.toFixed(2):"Unknown",l=e.popularity?e.popularity.toFixed(2):"Unknown",h=e.overview||"There is no information about this movie.",b=n(e.genre_ids);s.innerHTML=`
       <div class="poster-container">
-        <img src="${i}" alt="${e.title||e.original_title||"Movie Poster"}">
+        <img src="${c}" alt="${e.title||e.original_title||"Movie Poster"}">
       </div>
       <div class="movie-details">
         <h3>${e.title||e.original_title}</h3>
         <div class="movie-details-info">
-         <p>Release date: <span class="release-date-text">${a}</span></p>
+         <p>Release date: <span class="release-date-text">${y}</span></p>
 
-         <p>Vote / Votes: <span class ="vote-text">${l}</span> / <span class="vote-text">10</span></p>
-         <p>Popularity: ${b}</p>
-         <p>Genre: ${f}</p>
+         <p>Vote / Votes: <span class ="vote-text">${i}</span> / <span class="vote-text">10</span></p>
+         <p>Popularity: ${l}</p>
+         <p>Genre: ${b}</p>
         </div>
-        <p class="about-text">ABOUT ${y}</p>
+        <p class="about-text">ABOUT ${h}</p>
         <button class="add-to-library-btn" data-movie-id="${e.id}">Add to my library</button>
       </div>
-    `;const p=o.querySelector(".add-to-library-btn");n(e,p),p.addEventListener("click",h=>{d(e,p)}),w.appendChild(o)}u()});
+    `;const v=s.querySelector(".add-to-library-btn");g(e,v),v.addEventListener("click",p=>{u(e,v)}),k.appendChild(s)}d()});
 //# sourceMappingURL=index.js.map
